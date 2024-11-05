ebd61d53c9333794824f4f6df0e19d5296105a6d
cjy <1404340013@qq.com>
2024-11-05 14:49:36 +0800

    feat:服务端表单验证

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -86,3 +86,11 @@ export type InvoiceForm = {
   amount: number;
   status: 'pending' | 'paid';
 };
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
